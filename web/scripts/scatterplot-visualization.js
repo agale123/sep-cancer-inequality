1e35535adaa7b5001a3ef2781fcb80613fa7e7f0
Alison Gale <agale753@gmail.com>
2023-01-08 13:53:21 -0800

    Add regression line to scatterplot and all the metrics in the county dataset

--- web/scripts/scatterplot-visualization.js
+++ web/scripts/scatterplot-visualization.js
@@ -1,6 +1,18 @@
 // Set the dimensions and margins of the graph
-const s_margin = { top: 30, right: 30, bottom: 60, left: 60 };
-const buffer = 0.05;
+const MARGIN = { top: 30, right: 30, bottom: 60, left: 60 };
+
+/**
+ * 
+ * @param {Array<Object>} data 
+ * @param {string} metric
+ * @returns {number[]} A two-element array with the range that should be used.
+ */
+function getDataRange(data, metric) {
+    const min = d3.min(data, (d) => d[metric]);
+    const max = d3.max(data, (d) => d[metric]);
+
+    return [min, max];
+}
 
 /**
  * Update the scatterplot to show the relationship between the two variables.
@@ -14,19 +26,16 @@ async function updateScatterplot(xMetric, yMetric) {
     // Get the width for the scatterplot element.
     const clientWidth =
         document.getElementById("canvas_scatterplot").clientWidth;
-    const s_width = clientWidth - s_margin.left - s_margin.right;
-    const s_height = 400 - s_margin.top - s_margin.bottom;
+    const w = clientWidth - MARGIN.left - MARGIN.right;
+    const h = 400 - MARGIN.top - MARGIN.bottom;
 
     // Attach an svg to the Append the svg object to the body of the page
     const scatterplot = d3.select("#canvas_scatterplot")
         .append("svg")
-        .attr("width",
-            s_width + s_margin.left + s_margin.right)
-        .attr("height",
-            s_height + s_margin.top + s_margin.bottom)
+        .attr("width", w + MARGIN.left + MARGIN.right)
+        .attr("height", h + MARGIN.top + MARGIN.bottom)
         .append("g")
-        .attr("transform",
-            `translate(${s_margin.left}, ${s_margin.top})`);
+        .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`);
 
     // Read the data
     let data = await getData([xMetric, yMetric]);
@@ -35,50 +44,69 @@ async function updateScatterplot(xMetric, yMetric) {
     data = data.filter(d => d[xMetric] && d[yMetric]);
 
     // Add X axis
-    const xMin = d3.min(data, (d) => d[xMetric]);
-    const xMax = d3.max(data, (d) => d[xMetric]);
-    const x = d3.scaleLinear()
-        .domain([
-            xMin - buffer * (xMax - xMin),
-            xMax + buffer * (xMax - xMin)
-        ])
-        .range([0, s_width]);
+    const xRange = getDataRange(data, xMetric);
+    const x = d3.scaleLinear().domain(xRange).range([0, w]);
     scatterplot.append("g")
-        .attr("transform", `translate(0,${s_height})`)
+        .attr("transform", `translate(0,${h})`)
         .call(d3.axisBottom(x).tickFormat(d3.format("~s")));
     scatterplot.append("text")
         .attr("text-anchor", "middle")
-        .attr("x", s_width / 2)
-        .attr("y", s_height + s_margin.top + 15)
+        .attr("x", w / 2)
+        .attr("y", h + MARGIN.top + 15)
         .text(getMetricLabel(xMetric));
 
     // Add Y axis
-    const yMin = d3.min(data, (d) => d[yMetric]);
-    const yMax = d3.max(data, (d) => d[yMetric]);
     const y = d3.scaleLinear()
-        .domain([
-            yMin - buffer * (yMax - yMin),
-            yMax + buffer * (yMax - yMin)
-        ])
-        .range([s_height, 0]);
+        .domain(getDataRange(data, yMetric)).range([h, 0]);
     scatterplot.append("g").call(d3.axisLeft(y).tickFormat(d3.format("~s")));
     scatterplot.append("text")
         .attr("text-anchor", "center")
         .attr("transform", "rotate(-90)")
-        .attr("x", -(s_margin.top + s_margin.bottom + s_height / 2))
-        .attr("y", -s_margin.left + 20)
+        .attr("x", -(MARGIN.top + MARGIN.bottom + h / 2))
+        .attr("y", -MARGIN.left + 20)
         .text(getMetricLabel(yMetric))
 
     // Add dots
-    scatterplot.append('g')
+    scatterplot.append("g")
         .selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", d => x(parseFloat(d[xMetric])))
-        .attr("cy", d => y(parseFloat(d[yMetric])))
+        .attr("cx", d => x(d[xMetric]))
+        .attr("cy", d => y(d[yMetric]))
         .attr("r", 1.5)
         .style("fill", "#69b3a2");
+
+    // Add a clipPath to prevent the regression from extending outside the axes.
+    scatterplot.append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", w)
+        .attr("height", h);
+
+    // Add linear regression line
+    const linReg = d3.regressionLinear()
+        .x(d => d[xMetric])
+        .y(d => d[yMetric])
+        .domain(xRange);
+    scatterplot.append("g").append("line")
+        .attr("class", "regression")
+        .datum(linReg(data))
+        .attr("x1", d => x(d[0][0]))
+        .attr("x2", d => x(d[1][0]))
+        .attr("y1", d => y(d[0][1]))
+        .attr("y2", d => y(d[1][1]))
+        .attr("stroke-width", 1)
+        .attr("stroke", "black")
+        .attr("clip-path", "url(#clip)");
+
+
+    // Render a text summary of the relationship
+    document.getElementById("scatter_relationship").innerHTML = `
+        Use this scatterplot to understand how these two variables are 
+        correlated. Based on the slope of the line, there is a
+        <b>${linReg(data).a > 0 ? "positive" : "negative"}</b> relationship
+        between the variables.`;
 
     // Render metric descriptions
     document.getElementById("scatter_x").innerHTML =
